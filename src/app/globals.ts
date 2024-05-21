6779401f1c0e2b8be7b965c54c3854d1e67a2f2f
sameh-ali12 <119759726+sameh-ali12@users.noreply.github.com>
2024-05-21 16:02:39 +0300

    Update globals.ts

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -4,7 +4,7 @@ import { Injectable } from "@angular/core";
 export class Globals {
 ///   url = 'http://localhost:3000/';
  // url = 'https://tame-blue-leopard-suit.cyclic.app';
-   url = 'https://backendvicky.onrender.com';
+   url = 'https://vickybackend.onrender.com';
 
 
   // url = "https://salesmobile.herokuapp.com/";
